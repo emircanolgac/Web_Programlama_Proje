5c7cb2b137f0f2a31909d169e97f4dce920e4cbe
Emir Can Olgac <62564166+emircanolgac@users.noreply.github.com>
2021-12-11 12:41:39 +0300

    admin paneli eklendi

--- Proje/Controllers/HomeController.cs
+++ Proje/Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace Proje.Controllers
             return View();
         }
 
+        public IActionResult Admin_Login()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
